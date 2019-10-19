75242d49d53d35ec8fd482b77c8e02ffcb2d8ef5
Salem Asrat <salemasrat@gmail.com>
2019-10-19 22:20:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -108,9 +108,6 @@ def all_supplies_in_holidays(holiday_hash)
   end 
 end
 
-all_supplies_in_holidays(holiday_supplies)
-
-
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
